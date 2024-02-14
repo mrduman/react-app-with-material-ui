fc7a5585b8ded1cbe6a2c7abb625b33859e57633
mrduman <omer.duman@gmx.de>
2024-02-14 20:31:32 +0100

    components create wip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import { Box, Stack } from "@mui/material";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="App">Welcome</div>;
+  return (
+    <Box>
+      <Navbar />
+      <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
+  );
 }
 
 export default App;
